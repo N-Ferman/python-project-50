e49fab5f4c6a148ce5599d6e88fa2b0beefa3ad2
Ferman <nferman01@gmail.com>
2026-02-10 13:39:05 +0200

    2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,11 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    args = parser.parse_args()
+
+    parser.add_argument('-f', '--format', default='stylish',
+                        help='set format of output (default: stylish)') 
+    
+    parser.parse_args()
 
 if __name__ == "__main__":    
     main()
